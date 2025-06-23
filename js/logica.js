9ce7f0140883286b6a7b77cf66af1733e65c93e5
ingrid <ingridledesma22@gmail.com>
2025-06-23 00:58:02 -0300

    vista de home en proceso

---
+++ js/logica.js
@@ -0,0 +1,66 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btnMenu = document.querySelector(".btn_menu");
+    const menuList = document.querySelector(".lista-container-nav");
+
+    btnMenu.addEventListener("click", () => {
+        menuList.classList.toggle("show");
+    });
+
+    const formulario = document.querySelector(".form");
+    const submitBtn = formulario.querySelector(".btn");
+    const errorMsg = formulario.querySelector(".error-msg");
+
+    formulario.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const telefono = document.getElementById("telefono").value.trim();
+        const email = document.getElementById("email").value.trim();
+        const mensaje = document.getElementById("mensaje").value.trim();
+        const inputOculto = document.getElementById("oculto").value.trim();
+
+        submitBtn.classList.remove("click", "error");
+        errorMsg.classList.remove("show");
+
+
+    // Validar si el campo oculto fue completado (posible bot)
+    if (inputOculto !== "") {
+        console.warn("Posible bot detectado. Envío cancelado.");
+        return; // Cancela el envío silenciosamente
+    }
+
+
+
+        if (telefono === "" || email === "" || mensaje === "") {
+            // Mostrar cruz roja y mensaje de error
+            submitBtn.classList.add("error");
+            errorMsg.classList.add("show");
+        } else {
+
+        
+            // Mostrar tilde verde
+            submitBtn.classList.add("click");
+            submitBtn.disabled = true;
+
+            setTimeout(() => {
+                submitBtn.classList.remove("click");
+                submitBtn.disabled = false;
+                formulario.reset();
+            }, 2000);
+
+
+        }
+    });
+});
+document.querySelector(".btn-contact").addEventListener("click", e => {
+  e.preventDefault();
+  document.getElementById("modal").classList.add("open");
+  document.getElementById("overlay").classList.add("open");
+});
+document.getElementById("closeModal").addEventListener("click", () => {
+  document.getElementById("modal").classList.remove("open");
+  document.getElementById("overlay").classList.remove("open");
+});
+document.getElementById("overlay").addEventListener("click", () => {
+  document.getElementById("modal").classList.remove("open");
+  document.getElementById("overlay").classList.remove("open");
+});
